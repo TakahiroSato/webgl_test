c99f79c43ecbc50130d7780df8eca371b0e4b959
TakahiroSato <futurenova52@gmail.com>
2018-04-26 21:10:30 +0900

    eslint設定

--- js/0405.js
+++ js/0405.js
@@ -1,3 +1,4 @@
+/* globals thrjs2d, $ */
 var threejs = thrjs2d;
 threejs.init("canvas");
 
@@ -12,9 +13,9 @@ var addCnt = 0;
 var cnt = 0;
 
 var mousePos = {
-    x:0,
-    y:0
-}
+	x:0,
+	y:0
+};
 
 threejs.moveTo(0, 0);
 threejs.lineTo(100, 100);
@@ -22,30 +23,30 @@ threejs.lineTo(100, 100);
 threejs.fillRect(100, 100, 100, 100);
 
 $("#canvas").on("mousemove", function(e){
-    mousePos.x = e.clientX;
-    mousePos.y = -e.clientY;
+	mousePos.x = e.clientX;
+	mousePos.y = -e.clientY;
 });
 
 $("#canvas").on("click", function(){
-    addX = Math.PI/50;
-    addCnt = 1;
+	addX = Math.PI/50;
+	addCnt = 1;
 });
 
 (function reload(){
-    x += addX;
-    cnt += addCnt;
-    if(cnt % 25 == 0){
-        addX = 0;
-        addCnt = 0;
-    }
-    if(!box.isEmpty()){
-        box.setPos(mousePos.x, mousePos.y, 0);
-        box.setRotation(x, y, z);
-    }
-    if(!box2.isEmpty()){
-        box2.setPos(mousePos.x+100, mousePos.y+100, 0);
-        box2.setRotation(x, y, z);
-    }
-    threejs.render();
-    requestAnimationFrame(reload);
+	x += addX;
+	cnt += addCnt;
+	if(cnt % 25 == 0){
+		addX = 0;
+		addCnt = 0;
+	}
+	if(!box.isEmpty()){
+		box.setPos(mousePos.x, mousePos.y, 0);
+		box.setRotation(x, y, z);
+	}
+	if(!box2.isEmpty()){
+		box2.setPos(mousePos.x+100, mousePos.y+100, 0);
+		box2.setRotation(x, y, z);
+	}
+	threejs.render();
+	requestAnimationFrame(reload);
 })();
--- js/thrjs2d.js
+++ js/thrjs2d.js
@@ -1,152 +1,154 @@
+/* globals THREE, $ */
+
 var thrjs2d = (function() {
-    var renderer;
-    var scene;
-    var camera;
-    var div;
-    var width;
-    var height;
-    var fov = 45;       // 画角
-    var near = 0.1;     // 視体積手前までの距離
-    var far = 1000;     // 視体積奥までの距離
-    var sx;
-    var sy;
-    var backGroundColor = "0x000000";
+	var renderer;
+	var scene;
+	var camera;
+	var div;
+	var width;
+	var height;
+	var fov = 45;       // 画角
+	var near = 0.1;     // 視体積手前までの距離
+	var far = 1000;     // 視体積奥までの距離
+	var sx;
+	var sy;
+	var backGroundColor = "0x000000";
 
-    var objectsArray = [];
+	var objectsArray = [];
 
-    function createEmptyObject(){
-        return {
-            mesh: null,
-            geometry: null,
-            material: null,
-            texture: null,
-            setPos: function(x, y, z){
-                this.mesh.setPos(x, y, z);
-            },
-            setRotation: function(x, y, z){
-                this.mesh.setRotation(x, y, z);
-            },
-            remove: function(){
-                if(objectsArray.indexOf(this) != -1){
-                    objectsArray.splice(objectsArray.indexOf(this), 1);
-                }
-                if(this.mesh !== null) scene.remove(this.mesh);
-                if(this.geometry !== null) this.geometry.dispose();
-                if(this.material !== null) this.material.dispose();
-                if(this.texture !== null) this.texture.dispose();
+	function createEmptyObject(){
+		return {
+			mesh: null,
+			geometry: null,
+			material: null,
+			texture: null,
+			setPos: function(x, y, z){
+				this.mesh.setPos(x, y, z);
+			},
+			setRotation: function(x, y, z){
+				this.mesh.setRotation(x, y, z);
+			},
+			remove: function(){
+				if(objectsArray.indexOf(this) != -1){
+					objectsArray.splice(objectsArray.indexOf(this), 1);
+				}
+				if(this.mesh !== null) scene.remove(this.mesh);
+				if(this.geometry !== null) this.geometry.dispose();
+				if(this.material !== null) this.material.dispose();
+				if(this.texture !== null) this.texture.dispose();
 
-                this.mesh = null;
-                this.geometry = null;
-                this.material = null;
-                this.texture = null;
-            },
-            isEmpty: function(){
-                return this.mesh === null ? true : false;
-            }
-        }
-    }
+				this.mesh = null;
+				this.geometry = null;
+				this.material = null;
+				this.texture = null;
+			},
+			isEmpty: function(){
+				return this.mesh === null ? true : false;
+			}
+		}
+	}
 
-    return retObj = {
-        fillStyle:0,
-        lineWidth:1,
-        globalAlpha:1,
-        init:function(divid){
-            div = document.getElementById(divid);
-            width = div.clientWidth;
-            height = div.clientHeight;
-            renderer = new THREE.WebGLRenderer({antialias: false, alpha:true}); // レンダラーの生成
-            renderer.setSize(width, height); // レンダラーのサイズをdivのサイズに設定
-            renderer.setClearColor(backGroundColor, 1); // レンダラーの背景色を黒色（透過）に設定
-            div.appendChild(renderer.domElement); // div領域にレンダラーを配置
-            scene = new THREE.Scene();  // シーンの生成
-            // 座標軸を表示
-            var axes = new THREE.AxesHelper(width);
-            scene.add(axes);
-            var directionalLight = new THREE.DirectionalLight( 0xffffff );
-            directionalLight.position.set( 0, 0.7, 0.7 );
-            scene.add( directionalLight );
-            //camera = new THREE.PerspectiveCamera(fov, width/height, near, far);
-            camera = new THREE.OrthographicCamera(width/-2,width/2, height/2,height/-2, near, far);
-            camera.up.set(0,1,0);
-            camera.position.set(0,0,height/2);
-            camera.lookAt(new THREE.Vector3(0, 0, 0));
-        },
-        setCameraPosition:function(x, y, z){
-            camera.position.set(-x,y,height/2+z);
-        },
-        moveTo:function(x, y){
-            this.sx = x;
-            this.sy = y;
-        },
-        lineTo:function(x, y){
-            this.drawLine(this.sx, this.sy, x, y, this.lineWidth, this.fillStyle);
-            this.sx = x;
-            this.sy = y;
-        },
-        drawLine:function(sx, sy, dx, dy, w, color){
-            dy *= -1;
-            sy *= -1;
-            var obj = createEmptyObject();
-            // geometryの宣言と生成
-            obj.geometry = new THREE.Geometry();
-            // 頂点座標の追加
-            obj.geometry.vertices.push(new THREE.Vector3(sx-width/2, sy+height/2, 0));
-            obj.geometry.vertices.push(new THREE.Vector3(dx-width/2, dy+height/2, 0));
-            // 線オブジェクトの生成
-            obj.material = new THREE.LineBasicMaterial({linewidth:w, color: color})
-            obj.mesh = new THREE.Line(obj.geometry, obj.material);
-            objectsArray.push(obj);
-            // sceneにlineを追加
-            scene.add(obj.mesh);
-        },
-        fillRect:function(x, y, w, h){
-            return this.drawRect(x, y, w, h, this.fillStyle);
-        },
-        drawRect:function(x, y, w, h, color){
-            y *= -1;
-            var obj = createEmptyObject();
-            obj.geometry = new THREE.PlaneGeometry(w, h);
-            obj.material = new THREE.MeshBasicMaterial({color: color});
-            obj.material.transparent = true;
-            obj.material.opacity = this.globalAlpha;
-            obj.mesh = new THREE.Mesh(obj.geometry, obj.material);
-            obj.mesh.position.x = x-(width-w)/2;
-            obj.mesh.position.y = y+(height-h)/2;
-            obj.mesh.position.z = 0;
-            objectsArray.push(obj);
-            scene.add(obj.mesh);
-            return obj;
-        },
-        drawBox:function(x, y, w, h, d, color){
-            y *= -1;
-            var obj = createEmptyObject();
-            obj.geometry = new THREE.BoxGeometry(w, h, d);
-            obj.material = new THREE.MeshLambertMaterial({color: color});
-            obj.material.transparent = true;
-            obj.material.opacity = this.globalAlpha;
-            obj.mesh = new THREE.Mesh(obj.geometry, obj.material);
-            scene.add(obj.mesh);
-            obj.mesh.setPos = function(x, y, z){
-                this.position.x = x-(width-w)/2;
-                this.position.y = y+(height-h)/2;
-                this.position.z = z;
-            }
-            obj.mesh.setRotation = function(x, y, z){
-                this.rotation.x = x;
-                this.rotation.y = y;
-                this.rotation.z = z;
-            }
-            obj.mesh.setPos(x, y, z);
-            objectsArray.push(obj);
-            return obj;
-        },
-        clear:function(){
-            while(objectsArray.length > 0){
-                objectsArray[0].remove();
-            }
-        },
-        render:function(){
-            renderer.render(scene, camera);
-        }
-    };
+	return retObj = {
+		fillStyle:0,
+		lineWidth:1,
+		globalAlpha:1,
+		init:function(divid){
+			div = document.getElementById(divid);
+			width = div.clientWidth;
+			height = div.clientHeight;
+			renderer = new THREE.WebGLRenderer({antialias: false, alpha:true}); // レンダラーの生成
+			renderer.setSize(width, height); // レンダラーのサイズをdivのサイズに設定
+			renderer.setClearColor(backGroundColor, 1); // レンダラーの背景色を黒色（透過）に設定
+			div.appendChild(renderer.domElement); // div領域にレンダラーを配置
+			scene = new THREE.Scene();  // シーンの生成
+			// 座標軸を表示
+			var axes = new THREE.AxesHelper(width);
+			scene.add(axes);
+			var directionalLight = new THREE.DirectionalLight( 0xffffff );
+			directionalLight.position.set( 0, 0.7, 0.7 );
+			scene.add( directionalLight );
+			//camera = new THREE.PerspectiveCamera(fov, width/height, near, far);
+			camera = new THREE.OrthographicCamera(width/-2,width/2, height/2,height/-2, near, far);
+			camera.up.set(0,1,0);
+			camera.position.set(0,0,height/2);
+			camera.lookAt(new THREE.Vector3(0, 0, 0));
+		},
+		setCameraPosition:function(x, y, z){
+			camera.position.set(-x,y,height/2+z);
+		},
+		moveTo:function(x, y){
+			this.sx = x;
+			this.sy = y;
+		},
+		lineTo:function(x, y){
+			this.drawLine(this.sx, this.sy, x, y, this.lineWidth, this.fillStyle);
+			this.sx = x;
+			this.sy = y;
+		},
+		drawLine:function(sx, sy, dx, dy, w, color){
+			dy *= -1;
+			sy *= -1;
+			var obj = createEmptyObject();
+			// geometryの宣言と生成
+			obj.geometry = new THREE.Geometry();
+			// 頂点座標の追加
+			obj.geometry.vertices.push(new THREE.Vector3(sx-width/2, sy+height/2, 0));
+			obj.geometry.vertices.push(new THREE.Vector3(dx-width/2, dy+height/2, 0));
+			// 線オブジェクトの生成
+			obj.material = new THREE.LineBasicMaterial({linewidth:w, color: color})
+			obj.mesh = new THREE.Line(obj.geometry, obj.material);
+			objectsArray.push(obj);
+			// sceneにlineを追加
+			scene.add(obj.mesh);
+		},
+		fillRect:function(x, y, w, h){
+			return this.drawRect(x, y, w, h, this.fillStyle);
+		},
+		drawRect:function(x, y, w, h, color){
+			y *= -1;
+			var obj = createEmptyObject();
+			obj.geometry = new THREE.PlaneGeometry(w, h);
+			obj.material = new THREE.MeshBasicMaterial({color: color});
+			obj.material.transparent = true;
+			obj.material.opacity = this.globalAlpha;
+			obj.mesh = new THREE.Mesh(obj.geometry, obj.material);
+			obj.mesh.position.x = x-(width-w)/2;
+			obj.mesh.position.y = y+(height-h)/2;
+			obj.mesh.position.z = 0;
+			objectsArray.push(obj);
+			scene.add(obj.mesh);
+			return obj;
+		},
+		drawBox:function(x, y, w, h, d, color){
+			y *= -1;
+			var obj = createEmptyObject();
+			obj.geometry = new THREE.BoxGeometry(w, h, d);
+			obj.material = new THREE.MeshLambertMaterial({color: color});
+			obj.material.transparent = true;
+			obj.material.opacity = this.globalAlpha;
+			obj.mesh = new THREE.Mesh(obj.geometry, obj.material);
+			scene.add(obj.mesh);
+			obj.mesh.setPos = function(x, y, z){
+				this.position.x = x-(width-w)/2;
+				this.position.y = y+(height-h)/2;
+				this.position.z = z;
+			}
+			obj.mesh.setRotation = function(x, y, z){
+				this.rotation.x = x;
+				this.rotation.y = y;
+				this.rotation.z = z;
+			}
+			obj.mesh.setPos(x, y, z);
+			objectsArray.push(obj);
+			return obj;
+		},
+		clear:function(){
+			while(objectsArray.length > 0){
+				objectsArray[0].remove();
+			}
+		},
+		render:function(){
+			renderer.render(scene, camera);
+		}
+	};
 })();
